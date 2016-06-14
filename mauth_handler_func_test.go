97610de84e9b121b45e3113d7cabfef0cd60dfcb
Geoff Low <glow@mdsol.com>
2016-06-14 10:45:22 +0100

    Added makeRequest method for MAuthApp

--- mauth_handler_func_test.go
+++ mauth_handler_func_test.go
@@ -2,6 +2,44 @@ package go_mauth_client
 
 import "testing"
 
-func TestMAuthSignerHandler(t *testing.T) {
+//	"net/http"
+//	"net/http/httptest"
+//)
 
+/*
+Influenced by:
+https://elithrar.github.io/article/testing-http-handlers-go/
+*/
+
+//func TestMAuthSignerHandler(t *testing.T) {
+//	req, err := http.NewRequest("GET", "https://imedidata-sandbox.imedidata.net/api/v2/users.json", nil)
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//
+//	rr := httptest.NewRecorder()
+//
+//
+//
+//}
+
+func TestIsJsonNotJson(t *testing.T) {
+	no_json := "123"
+	is_json := isJSON(no_json)
+	if is_json != false {
+		t.Error("Not JSON, but thinks it is")
+	}
+}
+
+func TestIsJsonIsJson(t *testing.T) {
+	var no_json = "{\"number\": 123}"
+	is_json := isJSON(no_json)
+	if is_json != true {
+		t.Error("Is JSON, but thinks it is not")
+	}
+	no_json = "{\"number\": \"123\"}"
+	is_json = isJSON(no_json)
+	if is_json != true {
+		t.Error("Is JSON, but thinks it is not")
+	}
 }
