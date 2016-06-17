455052927bc1c5cb3ba6c523c7dc44f7e7f838c7
Geoff Low <glow@mdsol.com>
2016-06-17 23:22:35 +0100

    Upped coverage for methods Added missing fullURL use

--- go_mauth_client/mauth_client.go
+++ go_mauth_client/mauth_client.go
@@ -34,7 +34,11 @@ func (mauth_client *MAuthClient) fullURL(target_url string) (full_url string, er
 
 // MAuthClient.Get executes a GET request against a URL
 func (mauth_client *MAuthClient) Get(target_url string) (response *http.Response, err error) {
-	req, err := mauth_client.mauth_app.makeRequest("GET", target_url, "")
+	full_url, err := mauth_client.fullURL(target_url)
+	if err != nil {
+		return nil, err
+	}
+	req, err := mauth_client.mauth_app.makeRequest("GET", full_url, "")
 	if err != nil {
 		return nil, err
 	}
@@ -46,7 +50,11 @@ func (mauth_client *MAuthClient) Get(target_url string) (response *http.Response
 
 // MAuthClient.Delete executes a DELETE request against a URL
 func (mauth_client *MAuthClient) Delete(target_url string) (response *http.Response, err error) {
-	req, err := mauth_client.mauth_app.makeRequest("DELETE", target_url, "")
+	full_url, err := mauth_client.fullURL(target_url)
+	if err != nil {
+		return nil, err
+	}
+	req, err := mauth_client.mauth_app.makeRequest("DELETE", full_url, "")
 	if err != nil {
 		return nil, err
 	}
@@ -58,7 +66,11 @@ func (mauth_client *MAuthClient) Delete(target_url string) (response *http.Respo
 
 // MAuthClient.Post executes a POST request against a URL
 func (mauth_client *MAuthClient) Post(target_url string, data string) (response *http.Response, err error) {
-	req, err := mauth_client.mauth_app.makeRequest("POST", target_url, data)
+	full_url, err := mauth_client.fullURL(target_url)
+	if err != nil {
+		return nil, err
+	}
+	req, err := mauth_client.mauth_app.makeRequest("POST", full_url, data)
 	if err != nil {
 		return nil, err
 	}
@@ -70,7 +82,11 @@ func (mauth_client *MAuthClient) Post(target_url string, data string) (response 
 
 // MAuthClient.Put executes a PUT request against a URL
 func (mauth_client *MAuthClient) Put(target_url string, data string) (response *http.Response, err error) {
-	req, err := mauth_client.mauth_app.makeRequest("PUT", target_url, data)
+	full_url, err := mauth_client.fullURL(target_url)
+	if err != nil {
+		return nil, err
+	}
+	req, err := mauth_client.mauth_app.makeRequest("PUT", full_url, data)
 	if err != nil {
 		return nil, err
 	}
