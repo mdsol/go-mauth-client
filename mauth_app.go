bcc0456e5eab8b9373a6cb93de0f85b550b6312a
Geoff Low <glow@mdsol.com>
2019-03-06 09:59:51 +0000

    Removed python formatting removed duplicated code from LoadMauth Added check for pem Decoding

--- mauth_app.go
+++ mauth_app.go
@@ -1,10 +1,16 @@
 package go_mauth_client
 
 import (
+	"bytes"
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
+	"errors"
 	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
 )
 
 // MAuthApp struct holds all the necessary context for a MAuth App
@@ -14,37 +20,76 @@ type MAuthApp struct {
 }
 
 // LoadMauth loads the configuration  when the private key content is in a file
-func LoadMauth(app_id string, key_file_name string) (*MAuthApp, error) {
+func LoadMauth(appId string, keyFileName string) (*MAuthApp, error) {
 	// Create the MAuthApp struct
-	privateKey, err := ioutil.ReadFile(key_file_name)
+	keyFileContent, err := ioutil.ReadFile(keyFileName)
 	if err != nil {
 		return nil, err
 	}
 
-	block, _ := pem.Decode(privateKey)
-
-	privatekey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
-	if err != nil {
-		return nil, err
-	}
-
-	app := MAuthApp{AppId: app_id,
-		RsaPrivateKey: privatekey}
-	return &app, nil
+	// Reuse the core code
+	app, err := LoadMauthFromString(appId, keyFileContent)
+	return app, err
 }
 
 // LoadMauth loads the configuration  when the private key content is passed (such as from an environment string)
-func LoadMauthFromString(app_id string, key_file_content []byte) (*MAuthApp, error) {
+func LoadMauthFromString(appId string, keyFileContent []byte) (*MAuthApp, error) {
 	// Create the MAuthApp struct, when passed a byte array
 
-	block, _ := pem.Decode(key_file_content)
+	block, _ := pem.Decode(keyFileContent)
+	if block == nil {
+		return nil, errors.New("Unable to extract PEM content")
+	}
 
 	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
 	if err != nil {
 		return nil, err
 	}
 
-	app := MAuthApp{AppId: app_id,
+	app := MAuthApp{AppId: appId,
 		RsaPrivateKey: privateKey}
 	return &app, nil
+}
+
+// makeRequest formulates the message, including the MAuth Headers and returns a http.Request, ready to send
+func (mauthApp *MAuthApp) makeRequest(method string, rawurl string, body string,
+	extraHeaders map[string][]string) (req *http.Request, err error) {
+	// Use the url.URL to assist with path management
+	url2, err := url.Parse(rawurl)
+	if err != nil {
+		return nil, err
+	}
+	// this needs to persist
+	secondsSinceEpoch := time.Now().Unix()
+	// build the MWS string
+	stringToSign := MakeSignatureString(mauthApp, method, url2.Path, body, secondsSinceEpoch)
+	// Sign the string
+	signedString, err := SignString(mauthApp, stringToSign)
+	if err != nil {
+		return nil, err
+	}
+	// create a new request object
+	req, err = http.NewRequest(method, rawurl, bytes.NewBuffer([]byte(body)))
+	if err != nil {
+		return nil, err
+	}
+	// take everything and build the structure of the MAuth Headers
+	madeHeaders := MakeAuthenticationHeaders(mauthApp, signedString, secondsSinceEpoch)
+	for header, value := range madeHeaders {
+		req.Header.Set(header, value)
+	}
+	// Detect JSON, send appropriate Content-Type if detected
+	if isJSON(body) == true {
+		req.Header.Set("Content-Type", "application/json")
+	}
+	// Merge in any extra headers
+	for header, values := range extraHeaders {
+		for _, value := range values {
+			req.Header.Add(header, value)
+		}
+	}
+	// Add the User-Agent using the Client Version
+	req.Header.Set("User-Agent",
+		strings.Join([]string{"go-mauth-client", GetVersion()}, "/"))
+	return req, nil
 }
